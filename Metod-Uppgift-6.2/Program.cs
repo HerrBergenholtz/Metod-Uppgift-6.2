84532414b6201d5054f7150f69d2ee596fa20bb4
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-14 10:35:31 +0100

    Add project files.

---
+++ Metod-Uppgift-6.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_6_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Största(10, 20));
+        }
+
+        static int Största(int tal1, int tal2)
+        {
+            if (tal1 < tal2) return tal2;
+            else if (tal1 > tal2) return tal1;
+            else return tal1 + tal2;
+        }
+    }
+}
